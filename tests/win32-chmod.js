55abb8b666b7c0ba4dfb9a412fa11f8a9ee85ec4
Jed Richards <jedrichards@gmail.com>
2016-01-07 15:55:50 +0000

    Fix broken tests in modern Node

--- tests/win32-chmod.js
+++ tests/win32-chmod.js
@@ -11,43 +11,43 @@ var destDir = "./tmp/";
 var copiedFile = destDir+srcFile;
 
 exports.suite = vows.describe("Set defaul chmod for windows platform").addBatch({
-    "When the platform is not win32": {
+    "When not running under Windows": {
         topic: function() {
-            process.platform = 'linux';
             rsync({
                 src: srcFilePath,
                 dest: destDir,
-                noExec: true
+                noExec: true,
+                platform: 'darwin'
             },this.callback);
         },
-        "results in an rsync command that does not contains --chmod=ugo=rwX": function(error,stdout,stderr,cmd) {
+        "results in an rsync command that does not have --chmod=ugo=rwX automatically added": function(error,stdout,stderr,cmd) {
             assert.equal(cmd, 'rsync user@example.com:tests/fixtures/single.txt ./tmp/');
         }
     },
-    "When there is no existing chmod argument": {
+    "When running under Windows and there is no existing chmod argument": {
         topic: function() {
-            process.platform = 'win32';
             rsync({
                 src: srcFilePath,
                 dest: destDir,
-                noExec: true
+                noExec: true,
+                platform: 'win32'
             },this.callback);
         },
         "results in an rsync command that contains --chmod=ugo=rwX": function(error,stdout,stderr,cmd) {
             assert.equal(cmd, 'rsync user@example.com:tests/fixtures/single.txt ./tmp/ --chmod=ugo=rwX');
         }
     },
-    "When there is and existing chmod argument": {
+    "When running under Windows and there is already an existing chmod argument": {
         topic: function() {
-            process.platform = 'win32';
             rsync({
                 src: srcFilePath,
                 dest: destDir,
                 noExec: true,
-                args: ['--chmod=u=r']
+                args: ['--chmod=u=r'],
+                platform: 'win32'
             },this.callback);
         },
-        "results in an rsync command that does not override chmod argument": function(error,stdout,stderr,cmd) {
+        "results in an rsync command that does not override the chmod argument": function(error,stdout,stderr,cmd) {
             assert.equal(cmd, 'rsync user@example.com:tests/fixtures/single.txt ./tmp/ --chmod=u=r');
         }
     }
